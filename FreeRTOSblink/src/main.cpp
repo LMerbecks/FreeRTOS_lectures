dc54aa8cd6d8b3e597b8408f123e74a5d344b139
Leander Merbecks <leander.merbecks@gmx.de>
2026-04-28 13:52:14 +0200

    Added first project for lexture 1 where we blink the led

---
+++ FreeRTOSblink/src/main.cpp
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+#if CONFIG_FREERTOS_UNICORE
+static const BaseType_t app_cpu = 0;
+#else
+static const int app_cpu = 1;
+#endif
+
+static const int led_pin = GPIO_NUM_1;
+
+
+void toggleLED(void *parameter){
+  while(1){
+    digitalWrite(led_pin,HIGH);
+    vTaskDelay(500/ portTICK_PERIOD_MS);
+    digitalWrite(led_pin,LOW);
+    vTaskDelay(500/ portTICK_PERIOD_MS);
+  }
+}
+
+void setup() {
+  pinMode(led_pin, OUTPUT);
+
+  xTaskCreatePinnedToCore(
+    toggleLED,
+    "Blin LED 1s",
+    1024,
+    NULL,
+    1,
+    NULL,
+    app_cpu
+  );
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
